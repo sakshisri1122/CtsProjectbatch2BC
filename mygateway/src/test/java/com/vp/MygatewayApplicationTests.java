e90ccf5863c75c8888563733b438bb6f342ad6cc
sakshisri1122 <sakshisri2211@gmail.com>
2020-05-05 19:37:37 +0530

    Adding mygateway folder

---
+++ mygateway/src/test/java/com/vp/MygatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cts.project2bc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy  
+@EnableDiscoveryClient  
+public class ZuulgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApplication.class, args);
+	}
+
+}
